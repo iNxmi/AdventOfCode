1d25ba34431d391a27971643114cec14e7546e75
Memphis <memphispc70@gmail.com>
2024-12-04 00:52:20 +0100

    solved 2015 04

--- src/main/kotlin/com/nami/Main.kt
+++ src/main/kotlin/com/nami/Main.kt
@@ -6,6 +6,7 @@ val assignments = listOf(
     Y15D01(),
     Y15D02(),
     Y15D03(),
+    Y15D04(),
     Y23D01(),
     Y24D01(),
     Y24D02(),
---
+++ src/main/kotlin/com/nami/assignments/Y15D04.kt
@@ -0,0 +1,42 @@
+package com.nami.assignments
+
+import com.nami.Assignment
+import java.security.MessageDigest
+
+class Y15D04 : Assignment<String>(2015, 4) {
+
+    override fun getProcessedInput(raw: String): String {
+        return raw
+    }
+
+    @OptIn(ExperimentalStdlibApi::class)
+    private fun md5(input: String): String {
+        val md = MessageDigest.getInstance("MD5")
+        val digest = md.digest(input.toByteArray())
+        return digest.toHexString()
+    }
+
+    private fun crack(secret: String, prefix: String): Int {
+        var i = 0
+        while (true) {
+            val string = "$secret$i"
+            val hash = md5(string)
+
+            if (hash.startsWith(prefix))
+                break
+
+            i++
+        }
+
+        return i
+    }
+
+    override fun solveA(input: String): Int {
+        return crack(input, "00000")
+    }
+
+    override fun solveB(input: String): Int {
+        return crack(input, "000000")
+    }
+
+}
