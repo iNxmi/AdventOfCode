cecdb981823c608396ef17d8eaa46edee83f354e
Memphis <memphispc70@gmail.com>
2024-12-04 19:13:47 +0100

    solved 2024 04

--- src/main/kotlin/com/nami/Main.kt
+++ src/main/kotlin/com/nami/Main.kt
@@ -3,14 +3,16 @@ package com.nami
 import com.nami.assignments.*;
 
 val assignments = listOf(
-    Y15D01(),
-    Y15D02(),
-    Y15D03(),
-    Y15D04(),
-    Y23D01(),
-    Y24D01(),
-    Y24D02(),
-    Y24D03()
+//    Y15D01(),
+//    Y15D02(),
+//    Y15D03(),
+//    Y15D04(),
+//    Y15D05(),
+//to do    Y23D01(),
+//    Y24D01(),
+//    Y24D02(),
+//    Y24D03()
+    Y24D04()
 )
 
 val solutions = Assignment.getSolutions(assignments)
