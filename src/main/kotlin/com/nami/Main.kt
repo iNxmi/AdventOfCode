c1c0c856d8a766d956ba9f3ca959804e24bbda7d
Memphis <memphispc70@gmail.com>
2024-12-07 13:55:11 +0100

    misc

--- src/main/kotlin/com/nami/Main.kt
+++ src/main/kotlin/com/nami/Main.kt
@@ -7,12 +7,14 @@ val assignments = listOf(
 //    Y15D02(),
 //    Y15D03(),
 //    Y15D04(),
-//    Y15D05(),
-//to do    Y23D01(),
+//to do    Y15D05(),
+//   Y23D01(),
 //    Y24D01(),
 //    Y24D02(),
 //    Y24D03()
-    Y24D04()
+//    Y24D04()
+//to do    Y24D05()
+    Y24D06()
 )
 
 val solutions = Assignment.getSolutions(assignments)
