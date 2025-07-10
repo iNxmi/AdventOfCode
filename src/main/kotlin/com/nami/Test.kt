0378ba3f2deb8f9f9201d8259635be2a06ca07cf
Memphis <memphispc70@gmail.com>
2025-07-10 21:23:17 +0200

    Solved Y16D04.kt {A, B}

--- src/main/kotlin/com/nami/Test.kt
+++ src/main/kotlin/com/nami/Test.kt
@@ -1,5 +1,11 @@
 package com.nami
 
 fun main() {
+    val start = ('a').code
 
+    val base = 'v'
+    val offset = base.code - ('a').code
+
+    for (i in 0..100)
+        println("$i -> ${('a'.code + ((i + offset) % 26)).toChar()}")
 }
--- src/main/kotlin/com/nami/tools/Exporter.kt
+++ src/main/kotlin/com/nami/tools/Exporter.kt
@@ -48,6 +48,7 @@ class Exporter {
 
         return table.build().toString()
     }
+
 }
 
 fun main() {
