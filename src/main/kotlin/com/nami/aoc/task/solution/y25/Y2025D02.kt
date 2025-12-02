2b71873574c9b68ea43f5a53d9e9f8105468115c
Memphis Herghelegiu <memphispc70@gmail.com>
2025-12-02 06:23:50 +0100

    Solved Y2025D02.kt {A, B}

---
+++ src/main/kotlin/com/nami/aoc/task/solution/y25/Y2025D02.kt
@@ -0,0 +1,55 @@
+package com.nami.aoc.task.solution.y25
+
+import com.nami.aoc.print
+import com.nami.aoc.task.Part
+import com.nami.aoc.task.Task
+
+class Y2025D02 : Task<Set<LongRange>>(2025, 2) {
+
+    override fun getProcessedInput(raw: String) = raw.split(",").map { range ->
+        val split = range.split("-")
+        LongRange(split[0].toLong(), split[1].toLong())
+    }.toSet()
+
+    override fun getPartA() = object : Part.A<Set<LongRange>>(this) {
+
+        private val REGEX = Regex("(.+)\\1")
+
+        override fun solve(input: Set<LongRange>): Long {
+            var count = 0L
+            for (range in input) {
+                for (i in range) {
+                    val string = i.toString()
+                    if(REGEX.matches(string))
+                        count += i
+                }
+            }
+
+            return count
+        }
+
+    }
+
+    override fun getPartB() = object : Part.B<Set<LongRange>>(this) {
+        private val REGEX = Regex("(.+)\\1+")
+
+        override fun solve(input: Set<LongRange>): Long {
+            var count = 0L
+            for (range in input) {
+                for (i in range) {
+                    val string = i.toString()
+                    if(REGEX.matches(string))
+                        count += i
+                }
+            }
+
+            return count
+        }
+
+    }
+
+}
+
+//fun main() = Y2025D02().getTestVerifications().print()
+fun main() = Y2025D02().getResults().print()
+//fun main() = Y2025D02().getVerifications().print()
