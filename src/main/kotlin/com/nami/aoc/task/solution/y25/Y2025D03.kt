6b3e57cdd2131baf00ee8bd23c037762131012e8
Memphis Herghelegiu <memphispc70@gmail.com>
2025-12-03 07:12:00 +0100

    Backup

--- src/main/kotlin/com/nami/aoc/task/solution/y25/Y2025D03.kt
+++ src/main/kotlin/com/nami/aoc/task/solution/y25/Y2025D03.kt
@@ -3,88 +3,62 @@ package com.nami.aoc.task.solution.y25
 import com.nami.aoc.print
 import com.nami.aoc.task.Part
 import com.nami.aoc.task.Task
-import sun.swing.MenuItemLayoutHelper.max
-import kotlin.math.min
 
-class Y2025D03 : Task<List<Y2025D03.Bank>>(2025, 3) {
+class Y2025D03 : Task<Y2025D03.BankArray>(2025, 3) {
+
+    data class BankArray(
+        val banks: List<Bank>
+    )
 
     data class Bank(
         val batteries: List<Int>
     )
 
-    override fun getProcessedInput(raw: String) =
-        raw.lines().map { line -> Bank(line.map { char -> char.digitToInt() }) }
+    override fun getProcessedInput(raw: String) = BankArray(
+        raw.lines().map { line ->
+            Bank(line.map { char -> char.digitToInt() })
+        }
+    )
 
-    fun getJoltage(banks: List<Bank>, digits: Int): Long {
+    fun getJoltage(array: BankArray, digits: Int): Long {
         var sum = 0L
-        for (bank in banks) {
-
+        for (bank in array.banks) {
             val batteries = bank.batteries
-
-            val selectedBatteries = mutableListOf<Int>()
-            val selectedIndices = mutableListOf<Int>()
+            val selected = mutableListOf<Int>()
 
             var cursor = 0
             for (digit in 0..<digits) {
 
-                var selectedIndex = cursor
-                var selected = batteries[selectedIndex]
+                var batteryIndex = cursor
+                var battery = batteries[batteryIndex]
                 for (index in (cursor + 1)..(batteries.size - digits + digit)) {
                     val current = batteries[index]
-                    if (current > selected) {
-                        selected = current
-                        selectedIndex = index
+                    if (current > battery) {
+                        battery = current
+                        batteryIndex = index
                     }
                 }
 
-                selectedBatteries.add(selected)
-                selectedIndices.add(selectedIndex)
-
-                println("$selectedIndex=$selected ")
-
-                cursor = selectedIndex + 1
+                selected.add(battery)
+                cursor = batteryIndex + 1
             }
 
-            sum += selectedBatteries.joinToString("").toLong()
-
-
-//            val batteries = bank.batteries
-//
-//            var a = batteries[0]
-//            var aIndex = 0
-//            for (i in 1..<batteries.size - 1) {
-//                val current = bank.batteries[i]
-//
-//                if (current > a) {
-//                    a = current
-//                    aIndex = i
-//                }
-//            }
-//
-//            var b = batteries[aIndex + 1]
-//            for (i in (aIndex + 1)..<batteries.size) {
-//                val current = bank.batteries[i]
-//
-//                if (current > b)
-//                    b = current
-//            }
-//
-//            sum += "$a$b".toInt()
+            sum += selected.joinToString("").toLong()
         }
 
         return sum
     }
 
-    override fun getPartA() = object : Part.A<List<Bank>>(this) {
-        override fun solve(input: List<Bank>) = getJoltage(input, 2)
+    override fun getPartA() = object : Part.A<BankArray>(this) {
+        override fun solve(input: BankArray) = getJoltage(input, 2)
     }
 
-    override fun getPartB() = object : Part.B<List<Bank>>(this) {
-        override fun solve(input: List<Bank>) = getJoltage(input, 12)
+    override fun getPartB() = object : Part.B<BankArray>(this) {
+        override fun solve(input: BankArray) = getJoltage(input, 12)
     }
 
 }
 
 //fun main() = Y2025D03().getTestVerifications().print()
-fun main() = Y2025D03().getResults().print()
-//fun main() = Y2025D03().getVerifications().print()
+//fun main() = Y2025D03().getResults().print()
+fun main() = Y2025D03().getVerifications().print()
